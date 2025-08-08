7e34f7bf82f2e3a4b0bdde87f95f774649cbb3bd
Ryan Luong <lumintriet@gmail.com>
2025-08-08 09:32:29 +0700

    Setup shadcn/ui.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
